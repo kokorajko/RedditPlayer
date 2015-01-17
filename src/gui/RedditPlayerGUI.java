02442626b397c0746e2d6bb182f072f03f9346a0
Michał Gwóźdź <mic.gwo@gmail.com>
2015-01-17 17:21:48 +0100

    bugs fixed

--- src/gui/RedditPlayerGUI.java
+++ src/gui/RedditPlayerGUI.java
@@ -104,8 +104,10 @@ public class RedditPlayerGUI extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new RedditPlayerGUI().setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new RedditPlayerGUI().setVisible(true);
+            }
         });
     }
 
